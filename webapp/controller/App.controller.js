30c6fc17f2b917d7d67f1ee96489f5161c8baf74
Hakan AK <hakanak54@gmail.com>
2022-12-07 09:16:47 +0300

    buton isimlerini düzenledim

--- webapp/controller/App.controller.js
+++ webapp/controller/App.controller.js
@@ -6,19 +6,19 @@ sap.ui.define([
    return Controller.extend("sap.ui.demo.walkthrough.controller.App", {
       onShowHello : function () {
          // show a native JavaScript alert
-        // alert("Deneme Deneme");
+        alert("Deneme Deneme");
         MessageToast.show("This message should appear in the message toast", {
          duration: 5000,                  // default
          width: "60em",                   // default
-         my: "center bottom",             // default
-         at: "center bottom",             // default
+         my: "center center",             // default
+         at: "center center",             // default
          of: window,                      // default
          offset: "0 0",                   // default
          collision: "fit fit",            // default
          onClose: null,                   // default
          autoClose: true,                 // default
          animationTimingFunction: "ease", // default
-         animationDuration: 1000,         // default
+         animationDuration: 2000,         // default
          closeOnBrowserNavigation: true   // default
      });
       }
