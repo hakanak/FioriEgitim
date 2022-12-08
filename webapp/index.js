2068f8e0bb6fdcadfd954493f90d2cb4243d5a3c
Hakan AK <hakanak54@gmail.com>
2022-12-08 08:07:38 +0300

    Table Controller

--- webapp/index.js
+++ webapp/index.js
@@ -4,7 +4,7 @@ sap.ui.define([
 	"use strict";
 
 	XMLView.create({
-		viewName: "sap.ui.demo.walkthrough.view.App"
+		viewName: "sap.ui.demo.walkthrough.view.Table"
 	}).then(function (oView) {
 		oView.placeAt("content");
 	});
