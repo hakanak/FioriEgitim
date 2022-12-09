948f11bfeca6163cb2b1a7b43bd7bbf42fd6507c
Hakan AK <hakanak54@gmail.com>
2022-12-09 12:02:56 +0300

    Step 15: Nested Views - view çağırma

--- webapp/controller/Comp.Controller.js
+++ webapp/controller/Comp.Controller.js
@@ -4,14 +4,7 @@ sap.ui.define([
  ], function (Controller, MessageToast) {
     "use strict";
     return Controller.extend("sap.ui.demo.walkthrough.controller.Comp", {
-       onShowHello : function () {
-          // read msg from i18n model
-          var oBundle = this.getView().getModel("i18n").getResourceBundle();
-          var sRecipient = this.getView().getModel().getProperty("/recipient/name");
-          var sMsg = oBundle.getText("helloMsg", [sRecipient]);
-          // show message
-          MessageToast.show(sMsg);
-       }
+      
     });
  });
  
